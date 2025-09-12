e92c4900a915522ddd1efb03fdfe46e12122b769
TanishqDeshwal23 <139738414+TanishqDeshwal23@users.noreply.github.com>
2025-09-12 16:49:49 +0000

    comparison

---
+++ basics/comparison.js
@@ -0,0 +1,11 @@
+console.log("2">3);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+let check  = undefined;
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 4);
+console.log(undefined != check);
