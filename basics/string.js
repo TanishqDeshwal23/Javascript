2543c01161c096c88f2c37e5bdd3994cd8a5ca21
TanishqDeshwal23 <139738414+TanishqDeshwal23@users.noreply.github.com>
2025-09-16 05:16:35 +0000

    string methods

--- basics/string.js
+++ basics/string.js
@@ -1,7 +1,25 @@
 String = "hello mofos";
-console.log(String);
+// console.log(String);
 str = "mofossss";
-console.log(str);
+// console.log(str);
 add ="hi"+ " " + "hello";
-console.log(add);
+// console.log(add);
+let s = `this
+is a multiline 
+string`;
+// console.log(s);
 
+let s1 ="hello everyobe this is malik";
+s2 = s1.length;
+// console.log(s2);
+let string = "jai shankar maharaj";
+let string2 = string.substring(0,7);
+// console.log(string2);
+let searchstring = "this string needs to be searched";
+let i = searchstring.indexOf('searched');
+// console.log(i);
+let string3 =searchstring.replace('string' , 'number');
+// console.log(string3);
+let answer = '     this is the answer      ';
+let ans1 = answer.trim();
+console.log(ans1);
