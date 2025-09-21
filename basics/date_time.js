51f6579f3e0e35ef2ac729030ae0cb4b1512c769
TanishqDeshwal23 <139738414+TanishqDeshwal23@users.noreply.github.com>
2025-09-21 05:26:01 +0000

    basic date and time methods

---
+++ basics/date_time.js
@@ -0,0 +1,9 @@
+let mydate = new Date();
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString());
+const mycreateddate = new Date(2023, 0 , 23 , 6 ,3);
+// console.log(mycreateddate . toLocaleString());
+let mytimestamp = Date.now();
+// console.log(mytimestamp);
+console.log(mycreateddate.getTime());
