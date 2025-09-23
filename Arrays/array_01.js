65378e297bbcc252ca1d77591b075b73c2a5c679
TanishqDeshwal23 <139738414+TanishqDeshwal23@users.noreply.github.com>
2025-09-23 16:22:30 +0000

    array introduction

---
+++ Arrays/array_01.js
@@ -0,0 +1,12 @@
+const myArr = [0,1,5,4,8]; // method 1 for defining array
+// console.log(myArr);
+const myArr2 = new Array(1,55,66,77); // method 2 for defining array
+// console.log(myArr2);
+myArr2.push(87);
+// console.log(myArr2);
+myArr.pop(5);
+// console.log(myArr);
+myArr.unshift(9);
+// console.log(myArr);
+myArr.shift();
+console.log(myArr);
