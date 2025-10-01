f0b7431d71c580f6a43f18a92a10dbe33e27292c
TanishqDeshwal23 <139738414+TanishqDeshwal23@users.noreply.github.com>
2025-10-01 08:53:04 +0000

    json basic intro

---
+++ objectsJS/json Intro.js
@@ -0,0 +1,21 @@
+// {
+//     "name"="giyan",
+//     "age"="22",
+//     "isLoggedIn"="True"
+// }
+// // console.log({});
+
+let student = {
+  "id": 1,
+  "name": "Rahul",
+  "age": 20,
+  "skills": ["JS", "Python"]
+};
+
+// Print the whole object
+console.log(student);
+
+// Print specific values
+console.log(student.name);   
+console.log(student.skills);  
+console.log(student.skills[0]); 
