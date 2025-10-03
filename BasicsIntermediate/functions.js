3ca4adc757d5b203a57f4a29b1cb28c64da7b485
TanishqDeshwal23 <139738414+TanishqDeshwal23@users.noreply.github.com>
2025-10-03 06:38:04 +0000

    functions introduction

---
+++ BasicsIntermediate/functions.js
@@ -0,0 +1,16 @@
+// console.log("functions");
+// function sayName() {
+//     console.log("t"),
+//     console.log("a"),
+//     console.log("n"),
+//     console.log("u")
+// }
+// sayName();
+function addTwoNumbers(number1,number2){
+    console.log(number1 + number2);
+}
+addTwoNumbers(8,5);
+addTwoNumbers(7,5);
+addTwoNumbers(8,"5");
+addTwoNumbers(8,null);
+addTwoNumbers();
