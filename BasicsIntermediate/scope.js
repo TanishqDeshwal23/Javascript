8578e06d365659d0f40bc5c93a0cb247ead86388
TanishqDeshwal23 <139738414+TanishqDeshwal23@users.noreply.github.com>
2025-10-03 07:48:52 +0000

    scope in javascript and basic intro of hoisting

---
+++ BasicsIntermediate/scope.js
@@ -0,0 +1,49 @@
+// var c = 300
+// let a = 300
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     console.log("INNER: ", a);
+//     console.log("INNER: ", b);
+    
+// }
+// console.log(a);
+// // console.log(b);
+// console.log(c);
+
+// function one(){
+//     const username = "tanishq";
+//     function two(){
+//         const website = "gfg";
+//         console.log(username);
+//     }
+//     two();
+//     // console.log(website);
+// }
+// one();
+// // console.log(username);
+
+
+// if (true){
+//     const username = "tanu";
+//     if(username === "tanu"){
+//         const website = "gfg";
+//         console.log(username  + " " + website);
+//     }
+// }
+// console.log(website);
+
+//++++++++++++++interseting methods++++++++++++++++++++++
+// console.log(addone(5));
+// function addone(num1){
+//     return num1 + 1;
+// }
+// addone(5)
+// console.log(addone(5));
+
+// console.log(addTwo(5));
+const addTwo = function(num2){
+    return num2 + 2;
+}
+addTwo(5);
+console.log(addTwo(5));
