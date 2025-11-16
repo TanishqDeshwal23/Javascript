2f1c0521322f8f514896f45241eef91f950280c7
TanishqDeshwal23 <139738414+TanishqDeshwal23@users.noreply.github.com>
2025-11-16 08:18:01 +0000

    conditional statements basics

---
+++ controlFlow/first.js
@@ -0,0 +1,20 @@
+const age = 20;
+if(age > 18){
+    console.log("person is eligible to vote");
+}else{
+    console.log("not eligible");
+    
+}
+const price = 850;
+if(price <=200){
+    console.log("price is smaller than 250");
+    
+}
+else if(price >250 && price <=800){
+    console.log("price is between 250 and 800");
+    
+}
+else {
+    console.log("element greater thn 800");
+
+}
