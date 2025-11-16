1e92c20a46d594891b8ec3162ccf2b1e611c8c91
TanishqDeshwal23 <139738414+TanishqDeshwal23@users.noreply.github.com>
2025-11-16 08:20:54 +0000

    switch statements

---
+++ controlFlow/switch.js
@@ -0,0 +1,30 @@
+const month = "june";
+switch(month){
+    case "jan":
+    console.log("month is jan");
+    break;
+
+     case "feb":
+    console.log("month is feb");
+    break;
+
+     case "march":
+    console.log("month is march");
+    break;
+
+     case "april":
+    console.log("month is april");
+    break;
+
+     case "may":
+    console.log("month is may");
+    break;
+    
+     case "june":
+    console.log("month is june");
+    break;
+    default:
+    console.log("Month not found");    
+    break;
+    
+}
