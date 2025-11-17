f92880fb32c8a52123d8af5c20758d431f7fbb15
TanishqDeshwal23 <139738414+TanishqDeshwal23@users.noreply.github.com>
2025-11-17 04:55:27 +0000

    truthy statement in js

---
+++ controlFlow/truthy.js
@@ -0,0 +1,11 @@
+// TRUTHY VALUE : it is a value in javascript that treats it as true when it checks a condition.
+
+if("hello"){
+    console.log("executed successfully");
+    
+}
+let items = [];
+
+if (items) {
+  console.log("Array exists!");
+}
