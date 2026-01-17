c5eedaf6607245a11140d4b50817f582ff733653
TanishqDeshwal23 <139738414+TanishqDeshwal23@users.noreply.github.com>
2026-01-17 07:34:08 +0000

    Do while and while loop

---
+++ Iterations/two.js
@@ -0,0 +1,21 @@
+let index=0;
+while(index <= 220){
+    console.log(`value of index is ${index}`);
+    index=index+33;
+}
+
+
+let goal=1;
+do {
+    console.log(`player scored ${goal}`);
+    goal++;
+} while (goal<=10);
+
+
+let myArray = ["flash","batman","superman","hulk"];
+let n=myArray.length;
+let arr=0;
+while (arr < n) {
+    console.log(`value is ${myArray[arr]}`);
+    arr++;
+}
